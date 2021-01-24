3ce67b224b3cba4e8098c3ae50f7eb0c89b9b2bf
nathanielodazie <nathanielodazie@gmail.com>
2021-01-24 01:36:07 +0100

    fixed start

---
+++ config.js
@@ -0,0 +1,4 @@
+export const MONGODB =
+  "mongodb+srv://miles0069:iDslBdgHZovTEXaJ@cluster0.6wbxf.mongodb.net/milesDB?retryWrites=true&w=majority";
+
+export const SECRET_KEY = "some very secrete key";
